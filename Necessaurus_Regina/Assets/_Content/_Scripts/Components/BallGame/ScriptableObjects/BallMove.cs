91615a9d301903b303694a621d942eb591981274
Morne Gouws <webejame@gmail.com>
2024-06-15 14:53:42 +0200

    Added Scriptable Objects for ball moves

---
+++ Necessaurus_Regina/Assets/_Content/_Scripts/Components/BallGame/ScriptableObjects/BallMove.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ball Move", menuName = "Scriptables/BallGame/BallMove", order = 1)]
+
+public class BallMove : ScriptableObject
+{
+    public string MoveName;
+    public int MovePoints;
+    public string MoveDescription;
+}
