1a578c8d38b2790e8065dc2e5b3b12322efd6deb
Morne Gouws <webejame@gmail.com>
2024-06-22 21:42:18 +0200

    Fix timer and ball gravity on level end

--- Necessaurus_Regina/Assets/_Content/_Scripts/Managers/BallLevel/BallLevelManager.cs
+++ Necessaurus_Regina/Assets/_Content/_Scripts/Managers/BallLevel/BallLevelManager.cs
@@ -86,6 +86,9 @@ namespace BallGame.Managers
             OnLevelEnd?.Invoke();
 
             EventHub.PlayerEndGameScore( BallScoreManager.Instance.GetScore() );
+
+            timeRemaining = 0f;
+            BallGameUI.Instance.SetTimer(timeRemaining);
         }
     }   
 }
